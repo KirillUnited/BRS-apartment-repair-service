9d632c2dd09dfa129b165062685edc4fad2f60f9
k.dubov <example@example.com>
2023-09-21 18:18:19 +0300

    Update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,24 @@
 <template>
     <v-container class="d-flex justify-space-between align-center">
         <div class="logo d-flex align-center">
-            <img src="@/static/logo.svg" width="48" height="39"
-                 alt="Ремонт жилой и коммерческой нежвижимости в Минске"/>
+            <NuxtLink to="/" class="d-flex">
+                <img src="@/static/logo.svg" width="48" height="39"
+                     alt="Ремонт жилой и коммерческой нежвижимости в Минске"/>
+            </NuxtLink>
             <span class="logo-text text-secondary">Ремонт жилой и коммерческой<br/> нежвижимости в Минске</span>
         </div>
         <v-spacer></v-spacer>
-        <span class="text-secondary text-center">Связаться с нами<br/> через месседжеры</span>
+        <div class="socials d-flex flex-wrap align-center justify-center">
+            <span class="text-secondary text-center">Связаться с нами<br/> через месседжеры</span>
+            <ul class="socials-list">
+                <li v-for="social in socials" class="socials-list-item" :key="social">
+                    <NuxtLink to="https://www.google.com" class="d-flex align-center justify-center">
+                        <img :src="social.icon" class="socials-icon" width="24" height="24"
+                             :alt="social.name"/>
+                    </NuxtLink>
+                </li>
+            </ul>
+        </div>
         <v-spacer></v-spacer>
         <div class="d-flex align-center">
             <div class="contacts d-flex flex-column text-right mr-5">
@@ -23,10 +35,34 @@
 
 <script>
     import Button from "./Button";
+    import insta from "../assets/icons/insta.svg";
+    import youtube from "../assets/icons/youtube.svg";
+    import phone from "../assets/icons/phone.svg";
+    import telegram from "../assets/icons/telegram.svg";
 
     export default {
         name: "Header",
-        components: {Button}
+        components: {Button},
+        data: () => ({
+            socials: [
+                {
+                    icon: insta,
+                    name: "Instagram"
+                },
+                {
+                    icon: youtube,
+                    name: "youtube"
+                },
+                {
+                    icon: phone,
+                    name: "phone"
+                },
+                {
+                    icon: telegram,
+                    name: "telegram"
+                },
+            ]
+        }),
     }
 </script>
 
@@ -36,6 +72,27 @@
 
         &-text {
             font-size: 0.75rem;
+        }
+    }
+
+    .socials {
+        gap: 1rem;
+
+        &-list {
+            list-style: none;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            gap: 0.5rem;
+
+            &-item {
+                max-width: 2rem;
+                max-height: 2rem;
+            }
+        }
+
+        &-icon {
+            max-width: 100%;
         }
     }
 
