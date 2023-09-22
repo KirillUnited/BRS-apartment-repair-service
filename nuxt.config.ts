cd31ae296817faee1393d7436bf735cf78356e70
k.dubov <example@example.com>
2023-09-22 14:57:57 +0300

    Update assets folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  srcDir: 'src/',
-  devtools: { enabled: true },
-  head: {
-  },
-  css: [
-    "vuetify/styles",
-      "@/assets/styles/main.scss"
-  ],
-  build: {
-    transpile: ["vuetify"]
-  },
-  modules: [
-    '@nuxt/image-edge',
-  ],
-  image: {
-    dir: 'assets/images',
-    screens: {
-      'xs': 320,
-      'sm': 640,
-      'md': 768,
-      'lg': 1024,
-      'xl': 1280,
+    srcDir: 'src/',
+    devtools: {enabled: true},
+    head: {},
+    css: [
+        "vuetify/styles",
+        "@/assets/styles/main.scss"
+    ],
+    build: {
+        transpile: ["vuetify"]
     },
-  },
-  vite: {
-    define: {
-      "process.env.DEBUG": false
-    }
-  },
+    modules: [
+        '@nuxt/image-edge',
+    ],
+    image: {
+        dir: 'assets/images',
+        screens: {
+            'xs': 320,
+            'sm': 640,
+            'md': 768,
+            'lg': 1024,
+            'xl': 1280,
+        },
+    },
+    vite: {
+        define: {
+            "process.env.DEBUG": false
+        }
+    },
 })
