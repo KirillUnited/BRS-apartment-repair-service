fb44c005ca56da3e0e6ea8cfc7e12c12ffe48371
k.dubov <example@example.com>
2023-10-04 08:51:09 +0300

    Update call form

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,7 +6,7 @@
 
         <template v-slot:default="{ isActive }">
             <v-btn class="position-absolute" style="top: 0; right: 0; z-index: 10" icon="$close" variant="text" @click="isActive.value = false">
-                &#x2715
+                <v-icon icon="$close"></v-icon>
             </v-btn>
             <Call>
                 <template v-slot:actions>
